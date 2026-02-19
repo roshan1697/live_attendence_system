bdd94467ae0a3265ff06f487088fdf6c8304a37a
roshan1697 <roshanpoojary16@gmail.com>
2026-02-19 23:19:08 +0530

    --

--- backend/utils/user.ts
+++ backend/utils/user.ts
@@ -15,8 +15,8 @@ export class UserSession {
     initHandler = () => {
         this.ws.on('message', async(data)=>{
             const parse = JSON.parse(data.toString())
-            switch (parse.type) {
-                case 'join':
+            switch (parse.event) {
+                case 'JOIN':
                     // const token = parse.payload.token
                     // const userId = (Jwt.verify(token, process.env.JWT_SECRET || '') as JwtPayload).userId
                     // if(!userId){
@@ -29,7 +29,33 @@ export class UserSession {
                     })
                     
                     break;
-            
+                    case 'ATTENDANCE_MARKED':
+                        if(this.ws.user.role != 'teacher'){
+                            this.ws.close()
+                            return
+                        }
+
+                    break;   
+                    
+                    case 'TODAY_SUMMARY':
+                        if(this.ws.user.role != 'teacher'){
+                            this.ws.close()
+                            return
+                        }
+                    break;
+
+                    case 'MY_ATTENDANCE':
+                        if(this.ws.user.role != 'student'){
+                            this.ws.close()
+                            return
+                        }
+                    break;
+                    case 'DONE':
+                        if(this.ws.user.role != 'teacher'){
+                            this.ws.close()
+                            return
+                        }
+                    break;
                 default:
                     break;
             }
